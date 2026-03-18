4713c30012c1a99cce8166f818c0d881e74917fd
Dhruv Bhavsar <bhavsar.dhruv@gmail.com>
2026-03-18 19:23:35 +0530

    wokring on th emanual exection

--- src/pages/ManualExecutionPage.tsx
+++ src/pages/ManualExecutionPage.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import ManualExecution from "../components/ManualExecutionComponents/ManualExecution";
 import OpenPositions from "../components/ManualExecutionComponents/OpenPositions";
 import Orders from "../components/ManualExecutionComponents/Orders";
 
 export default function ManualExecutionPage() {
   return (
-    <div className="min-h-screen bg-slate-50 p-4">
+    <div className="max-h-screen bg-slate-50 p-4">
       <div className="flex flex-col lg:flex-row h-full">
         {/* LEFT COLUMN - 30% */}
         <div className="w-full lg:w-[42%] flex flex-col flex-shrink-0">
